a4a20fe88e38e2e823fef08b297db23b9fe35d12
Eskeptor <skyvvv624@naver.com>
2017-07-04 22:19:01 +0900

    add open and save(for unicode)

--- Openpad/Openpad/head.h
+++ Openpad/Openpad/head.h
@@ -6,11 +6,13 @@
 #include <WinSock2.h>
 #include <CommCtrl.h>
 
-
 #define CLASS_NAME		TEXT("OpenPad for Windows")
 #define CPARTS			2
 #define KOREAN			0x412
 #define ENGLISH			0x409
+#define NULL_TEXT		TEXT("")
+#define FILTER_FILE		TEXT("Text Files(*.txt)\0*.txt\0")
+#define FORMAT_TEXT		TEXT(".txt")
 
 HWND DoCreateStatusBar(HWND hwndParent, HMENU menu, HINSTANCE hinst, int cParts);
 void initMenu(HWND hWnd);
--- Openpad/Openpad/main.cpp
+++ Openpad/Openpad/main.cpp
@@ -6,6 +6,18 @@ int prevX, prevY;
 HINSTANCE langFile;
 LANGID curLangID = 0;
 
+OPENFILENAME fileDialog;
+TCHAR openFileName[MAX_PATH] = { 0, };
+wchar_t* editText;
+
+enum _DataChanged
+{
+	CHANGED = 0,
+	NOT_CHANGED = 1
+}DataChanged;
+
+// TODO: 할일
+
 INT APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE, TCHAR *pszLine, INT nShow)
 {
 	WNDCLASS wndClass;
@@ -55,16 +67,89 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	HMENU menu;
 	MENUITEMINFO itemInfo;
 
+	HANDLE file;
+
 	switch (iMsg)
 	{
 	case WM_CREATE:
 		initMenu(hWnd);
-		
+		DataChanged = NOT_CHANGED;
 		return 0;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
 		case ID_MENU_FILE_NEW:
+			if (DataChanged == CHANGED)
+			{
+				// TODO : 메시지박스에 넣을 문구 지역화하기
+				int select = MessageBox(hWnd, TEXT(""), TEXT(""), MB_YESNOCANCEL);
+				if (select == IDCANCEL)
+				{
+					break;
+				}			
+				else if (select == IDYES)
+				{
+					free(editText);
+				}
+			}
+			openFileName[0] = '\0';
+			SetWindowText(editBox, NULL_TEXT);
+			break;
+		case ID_MENU_FILE_OPEN:
+			if (DataChanged == NOT_CHANGED)
+			{
+				memset(&fileDialog, 0, sizeof(OPENFILENAME));
+				fileDialog.lStructSize = sizeof(OPENFILENAME);
+				fileDialog.hwndOwner = hWnd;
+				fileDialog.lpstrFilter = FILTER_FILE;
+				fileDialog.lpstrFile = openFileName;
+				fileDialog.nMaxFile = MAX_PATH;
+				// TODO : ANSI와 UTF-8 구분하기
+				if (GetOpenFileName(&fileDialog))
+				{
+					file = CreateFile(openFileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+					int fileSize = GetFileSize(file, NULL);
+					DWORD dwRead;
+					editText = (wchar_t*)malloc(sizeof(wchar_t) * fileSize);
+					ReadFile(file, editText, _tcslen(editText), &dwRead, NULL);
+					SetWindowText(editBox, editText);
+					CloseHandle(file);
+				}
+			}
+			else
+			{
+				int select = MessageBox(hWnd, TEXT(""), TEXT(""), MB_YESNOCANCEL);
+				if (select == IDCANCEL)
+				{
+					break;
+				}
+				else if (select == IDYES)
+				{
+					free(editText);
+				}
+			}
+			
+			break;
+		case ID_MENU_FILE_SAVE:
+			memset(&fileDialog, 0, sizeof(OPENFILENAME));
+			fileDialog.lStructSize = sizeof(OPENFILENAME);
+			fileDialog.hwndOwner = hWnd;
+			fileDialog.lpstrFilter = FILTER_FILE;
+			fileDialog.lpstrFile = openFileName;
+			fileDialog.nMaxFile = MAX_PATH;
+			if (GetSaveFileName(&fileDialog) != 0)
+			{
+				TCHAR* save = (TCHAR*)malloc(sizeof(TCHAR) * _tcslen(openFileName) + 4);
+				_tcscpy(save, openFileName);
+				_tcscat(save, FORMAT_TEXT);
+				file = CreateFile(save, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
+				int length = (GetWindowTextLength(editBox) + 1) * 2;
+				DWORD dwWritten;
+				editText = (wchar_t*)malloc(sizeof(wchar_t) * length);
+				GetWindowText(editBox, editText, length);
+				WriteFile(file, editText, length, &dwWritten, NULL);
+				CloseHandle(file);
+			}
 			break;
 		case ID_MENU_VIEW_STATUS:
 			menu = GetMenu(hWnd);
@@ -107,6 +192,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		return 0;
 	case WM_DESTROY:
 		FreeLibrary(langFile);
+		free(editText);
 		PostQuitMessage(0);
 	}
 	return DefWindowProc(hWnd, iMsg, wParam, lParam);
